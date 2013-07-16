8a3257c5509bdc25d69adc0acfff33e4da424f45
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-16 09:26:47 +0800

    change settings to include static bootstrap files and templates/ folder

--- internal/internal/settings.py
+++ internal/internal/settings.py
@@ -114,6 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'templates',
 )
 
 INSTALLED_APPS = (
@@ -127,6 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'bootstrap',
     'profiles',
     'projects',
 )
