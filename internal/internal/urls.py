661a642454fffcf57c6838f3998cc21cecf05a47
Angelique De Castro <ajdecast@ucsd.edu>
2013-07-18 19:25:37 -0700

    Revert "This reverts commit 20753fc70547f13b85e248aa07be61785ffba628."
    This reverts commit 55ed581d238f0bbb5cdd6c92011dff329ac9b63d.

--- internal/internal/urls.py
+++ internal/internal/urls.py
@@ -1,17 +1,29 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import *
+from projects.views import *
+from projects.admin import admin
+from projects.models import Project, Collaborator, Leader
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+# admin.autodiscover()
+
+"""create_proj_info = {
+	'queryset': Project.objects.all(),
+	'template_name': 'create_project.html',
+}"""
 
 urlpatterns = patterns('',
+	#url(r'^create_project/$', create_proj_info),
+	url(r'^create_project/$', create_project),
+	url(r'^project_index/$', project_index),
+	url(r'^project_details/$', project_details),
     # Examples:
-    # url(r'^$', 'internal.views.home', name='home'),
-    # url(r'^internal/', include('internal.foo.urls')),
+    # url(r'^$', 'kalite_internal.views.home', name='home'),
+    # url(r'^kalite_internal/', include('kalite_internal.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^home/', include(admin.site.urls)),
 )
