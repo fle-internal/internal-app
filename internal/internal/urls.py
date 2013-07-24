2fa80811b71215f1ee43ee6886ed7e4cf610d08f
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-24 09:08:32 -0700

    move profile and project templates to their own template directory

--- internal/internal/urls.py
+++ internal/internal/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import *
-from projects import views
 from projects.admin import admin
 from projects.models import Project, Collaborator, Leader
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from profiles import views
+import projects
+import profiles
 
 
 admin.autodiscover()
@@ -13,10 +13,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^profile_index/$', views.profile),
-    url(r'^profile/$', views.profile),
-    url(r'^create_project/$', views.create_project),
-    url(r'^project_index/$', views.project_index),
-    url(r'^project_details/$', views.project_details),
+    url(r'^profile_index/$', profiles.views.profile),
+    url(r'^profile/$', profiles.views.profile),
+    url(r'^create_project/$', projects.views.create_project),
+    url(r'^project_index/$', projects.views.project_index),
+    url(r'^project_details/$', projects.views.project_details),
     url(r'^home/', include(admin.site.urls)),
 )
