0e98369d2b0e04d5e40342a08e9bf4abe079ed8a
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-24 08:36:40 -0700

    cleanup urls

--- internal/internal/urls.py
+++ internal/internal/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import *
-from projects.views import *
+from projects import views
 from projects.admin import admin
 from projects.models import Project, Collaborator, Leader
 
@@ -14,16 +14,8 @@ from projects.models import Project, Collaborator, Leader
 
 urlpatterns = patterns('',
 	#url(r'^create_project/$', create_proj_info),
-	url(r'^create_project/$', create_project),
-	url(r'^project_index/$', project_index),
-	url(r'^project_details/$', project_details),
-    # Examples:
-    # url(r'^$', 'kalite_internal.views.home', name='home'),
-    # url(r'^kalite_internal/', include('kalite_internal.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+	url(r'^create_project/$', views.create_project),
+	url(r'^project_index/$', views.project_index),
+	url(r'^project_details/$', views.project_details),
 	url(r'^home/', include(admin.site.urls)),
 )
