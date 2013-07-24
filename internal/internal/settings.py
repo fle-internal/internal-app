6f5bd04cdea9148e756bb44e75f915209c79c873
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-24 08:28:09 -0700

    fix template path

--- internal/internal/settings.py
+++ internal/internal/settings.py
@@ -21,8 +21,8 @@ DATABASES = {
     }
 }
 
-#AUTH_USER_MODEL = 'profiles.TeamMember'
-#AUTH_PROFILE_MODULE = 'content.TeamMember'
+AUTH_USER_MODEL = 'profiles.TeamMember'
+AUTH_PROFILE_MODULE = 'content.TeamMember'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -114,7 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/angeliquedecastro/kalite-internal/internal/templates/internal',
+    'templates',
 )
 
 INSTALLED_APPS = (
