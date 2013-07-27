b04e9992847460f1beb5085320a5e4ab24b4f084
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-27 13:11:19 -0700

    cleanup and split internal/urls.py

--- internal/internal/urls.py
+++ internal/internal/urls.py
@@ -1,21 +1,15 @@
 from django.conf.urls import *
-from projects.admin import admin
-from projects.models import Project, Collaborator, Leader
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from projects import views as project_views
-from profiles import views as profile_views
+import profiles.urls
+import projects.urls
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^profile_index/$', profile_views.profile_index, name='profile_index'),
-    url(r'^profile/$', profile_views.profile),
-    url(r'^create_project/$', project_views.create_project),
-    url(r'^project_index/$', project_views.project_index),
-    url(r'^project_details/$', project_views.project_details),
-    url(r'^home/', include(admin.site.urls)),
+    url(r'^profiles/', include(profiles.urls)),
+    url(r'^projects/', include(projects.urls)),
 )
---
+++ internal/profiles/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import *
+
+from profiles import views
+
+urlpatterns = patterns('',
+    url(r'', views.profile_index, name='profile_index'),
+    url(r'/(?P<id>\d+)$', views.profile, name='profile_detail'),
+)
