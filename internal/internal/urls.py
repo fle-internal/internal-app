8d49dff4856e2c31f3b20e86e75ab48371d1e430
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-30 14:27:22 -0700

    make working logout logic

--- internal/internal/urls.py
+++ internal/internal/urls.py
@@ -10,7 +10,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name' : 'internal/login.html'}),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name' : 'internal/logout.html'}),
+    url(r'^logout/$', 'profiles.views.logout', name='logout'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^profiles/', include(profiles.urls)),
     url(r'^projects/', include(projects.urls)),
