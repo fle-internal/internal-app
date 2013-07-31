c23848d1083e527cea9193d14c1aba3ea686f1be
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-31 16:56:29 -0700

    added join model for TeamMember to Project, to hold the role that the TeamMember has

--- internal/projects/models.py
+++ internal/projects/models.py
@@ -8,9 +8,14 @@ class Plan(models.Model):
 class Project(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=50)
-    leader = models.ForeignKey(TeamMember, related_name="projects_led")
-    collaborators = models.ManyToManyField(TeamMember, related_name="projects")
+    owner = models.ForeignKey(TeamMember, related_name="projects_owned")
+    collaborators = models.ManyToManyField(TeamMember, related_name="projects", through='Role')
     startDate = models.DateField(max_length=10)
     deadLine = models.DateField(max_length=10)
     toDoList = models.ForeignKey('Plan')
     website = models.URLField()
+
+class Role(models.Model):
+    profile = models.ForeignKey(TeamMember)
+    project = models.ForeignKey(Project)
+    name = models.CharField(max_length=100)
