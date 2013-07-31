daf388a4b1230344c7c3c4abf59b4df1f0c3fa16
Angelique De Castro <ajdecast@ucsd.edu>
2013-07-31 14:54:23 -0700

    add unicode def to models

--- internal/projects/models.py
+++ internal/projects/models.py
@@ -1,35 +1,11 @@
 from django.db import models
-<<<<<<< HEAD
-import datetime
-# Create your models here.	
-    
-class Leader(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    def __unicode__(self):
-        return self.first_name
-	
-class Collaborator(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    def __unicode__(self):
-        return self.first_name
-    #Description = models.CharField(max_length=100)
-	#picture = models.URLField() //Low priority
-	#Roles = models.OneToManyField(Role)
-    #website = models.URLField()	#github page or anything that showcases the collaborator's background
-	#ProjectList = models.OneToManyField(Project)
-	#def createProject(self, keyword):
-	#def joinProject(self, keyword):"""
-=======
 from profiles.models import TeamMember
 # Create your models here.
 
 class Plan(models.Model):
     title = models.CharField(max_length=100)
->>>>>>> 00cdca17a23aca1a391b2e91407aef572037fcb0
+    def __unicode__(self):
+	return self.title
 
 class Project(models.Model):
     name = models.CharField(max_length=30)
@@ -40,9 +16,6 @@ class Project(models.Model):
     deadLine = models.DateField(max_length=10)
     toDoList = models.ForeignKey('Plan')
     website = models.URLField()
-<<<<<<< HEAD
-    def __unicode(self):
+    def __unicode__(self):
         return self.name
 
-=======
->>>>>>> 00cdca17a23aca1a391b2e91407aef572037fcb0
