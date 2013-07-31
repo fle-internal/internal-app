22aabb178b7805dc0aabb1da2f1a01f4c39a592b
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-31 16:06:57 -0700

    fix url regexp so profile_detail would come through

--- internal/profiles/urls.py
+++ internal/profiles/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import *
 from profiles import views
 
 urlpatterns = patterns('',
-    url(r'', views.profile_index, name='profile_index'),
+    url(r'^$', views.profile_index, name='profile_index'),
     url(r'(?P<id>\d+)/$', views.profile, name='profile_detail'),
 )
