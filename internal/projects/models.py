56f65c7afba7ce95bda492fcede4fcc54810d9c1
Angelique De Castro <ajdecast@ucsd.edu>
2013-07-31 14:22:26 -0700

    add unicode defs to models

--- internal/projects/models.py
+++ internal/projects/models.py
@@ -6,11 +6,15 @@ class Leader(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+    def __unicode__(self):
+        return self.first_name
 	
 class Collaborator(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+    def __unicode__(self):
+        return self.first_name
     #Description = models.CharField(max_length=100)
 	#picture = models.URLField() //Low priority
 	#Roles = models.OneToManyField(Role)
@@ -28,4 +32,6 @@ class Project(models.Model):
     deadline = models.DateField(max_length=10)
     #toDoList = models.ForeignKey(Plan)
     website = models.URLField()
+    def __unicode(self):
+        return self.name
 
