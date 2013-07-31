257c45e7ac8669c88f7b1c4c1d12681d581795b1
Aron Fyodor Asor <aronasorman@gmail.com>
2013-07-31 13:47:47 -0700

    remove leading backslash to make url tag work

--- internal/profiles/urls.py
+++ internal/profiles/urls.py
@@ -4,5 +4,5 @@ from profiles import views
 
 urlpatterns = patterns('',
     url(r'', views.profile_index, name='profile_index'),
-    url(r'/(?P<id>\d+)$', views.profile, name='profile_detail'),
+    url(r'(?P<id>\d+)/$', views.profile, name='profile_detail'),
 )
