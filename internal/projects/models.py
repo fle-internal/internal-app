5921f6ff02d34e933329cad2b266e780264a11f2
Aron Fyodor Asor <aronasorman@gmail.com>
2013-08-01 12:19:48 -0700

    refine project model fields

--- internal/projects/models.py
+++ internal/projects/models.py
@@ -2,20 +2,20 @@ from django.db import models
 from profiles.models import TeamMember
 # Create your models here.
 
-class Plan(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=100)
+    project = models.ForeignKey('Project', related_name='tasks')
 
 class Project(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=50)
     owner = models.ForeignKey(TeamMember, related_name="projects_owned")
     collaborators = models.ManyToManyField(TeamMember, related_name="projects", through='Role')
-    startDate = models.DateField(max_length=10)
-    deadLine = models.DateField(max_length=10)
-    toDoList = models.ForeignKey('Plan')
+    start_date = models.DateField(max_length=10)
+    deadline = models.DateField(max_length=10)
     website = models.URLField()
 
 class Role(models.Model):
     profile = models.ForeignKey(TeamMember)
     project = models.ForeignKey(Project)
-    name = models.CharField(max_length=100)
+    role_name = models.CharField(max_length=100)
