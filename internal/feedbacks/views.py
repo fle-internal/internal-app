d1b5fe35479dfe3ad0ce74c7c083d340f4a8ac62
Aron Fyodor Asor <aronasorman@gmail.com>
2013-08-06 13:32:49 -0700

    complete feedback adding logic

--- internal/feedbacks/views.py
+++ internal/feedbacks/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseForbidden
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseForbidden
+from django.shortcuts import render, redirect
 
 from feedbacks.forms import FeedbackCreationForm
+from feedbacks.models import Feedback
+from profiles.models import TeamMember
 from projects.models import Project
 
 @login_required
@@ -12,9 +14,27 @@ def create(request, project_id):
         return HttpResponseForbidden(request)
 
     if request.method == 'POST':
-        pass
-        # form = FeedbackCreationForm(request.POST)
-        # do something with the data
+        form = FeedbackCreationForm(request.POST)
+        form.fields['target'].queryset = project.collaborators.exclude(pk=request.user.id)
+        if form.is_valid():
+            data = form.cleaned_data
+            f = Feedback()
+            f.project = project
+            f.target = data['target']
+            f.maker = request.user
+            f.participation_rating = data['participation_rating']
+            f.contribution_rating = data['contribution_rating']
+            f.communication_rating = data['communication_rating']
+            f.ease_of_working_together_rating = data['ease_of_working_together_rating']
+            f.participation_rationale = data['participation_rationale']
+            f.contribution_rationale = data['contribution_rationale']
+            f.communication_rationale = data['communication_rationale']
+            f.ease_of_working_together_rationale = data['ease_of_working_together_rationale']
+            f.save()
+            # TODO: add message saying feedback creation is successful
+            return redirect('project_detail', id=project.id)
+        else:
+            return render(request, 'feedbacks/create.html', { 'form': form })
     elif request.method == 'GET':
         form = FeedbackCreationForm()
         form.fields['target'].queryset = project.collaborators.exclude(pk=request.user.id)
