35787be241b880939faa4cc569541245504bc7f7
Aron Fyodor Asor <aronasorman@gmail.com>
2013-08-06 11:28:09 -0700

    forbid user to create feedbacks for projects they are not part of

--- internal/feedbacks/views.py
+++ internal/feedbacks/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
 from feedbacks.forms import FeedbackCreationForm
@@ -7,12 +7,15 @@ from projects.models import Project
 
 @login_required
 def create(request, project_id):
+    project = Project.objects.get(pk=project_id)
+    if request.user not in project.collaborators.all():
+        return HttpResponseForbidden(request)
+
     if request.method == 'POST':
         pass
         # form = FeedbackCreationForm(request.POST)
         # do something with the data
     elif request.method == 'GET':
-        project = Project.objects.get(pk=project_id)
         form = FeedbackCreationForm()
         form.fields['target'].queryset = project.collaborators
         return render(request, 'feedbacks/create.html', { 'form': form })
