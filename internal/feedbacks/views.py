d15035b146955fe169305cb1a47df3c1a0812c37
Angelique De Castro <ajdecast@ucsd.edu>
2013-08-07 11:56:24 -0700

    merge

--- internal/feedbacks/views.py
+++ internal/feedbacks/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from feedbacks import models
 from django.views.generic import *
-from django.db.models import Avg
 
 class FeedbackList(ListView):
 	model = models.Feedback
 	context_object_name = 'feedback'
 	template_name = 'feedback/view_feedback.html'
+
+def summary(request, id):
+	person = TeamMember.objects.get(pk=id)
+	avg = models.Feedback.objects.all()
+	return render(request, 'profiles/profile.html', {'avg':avg, 'person':person})
