59de8b343bd5a7d481aa1b1683fb022880a449ff
Angelique De Castro <ajdecast@ucsd.edu>
2013-08-08 19:44:30 -0700

    add in class which computes average stars

--- internal/feedbacks/models.py
+++ internal/feedbacks/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Avg
+from django.db.models import *
 
 from profiles.models import TeamMember
 from projects.models import Project
@@ -19,10 +19,19 @@ class Feedback(models.Model):
     ease_of_working_together_rating = models.IntegerField(choices=RATING_CHOICES)
     ease_of_working_together_rationale = models.TextField()
 
-    """avg_participation = Feedback.objects.all().aggregate(Avg('participation_rating'))
-    avg_contribution = Feedback.objects.all().aggregate(Avg('contribution_rating'))
-    avg_communication = Feedback.objects.all().aggregate(Avg('communication_rating'))
-    avg_ease = Feedback.objects.all().aggregate(Avg('ease_of_working_together_rating'))
+class Avg(Feedback):
+    avg_participation = Feedback.objects.aggregate(Avg('participation_rating'))
+    avg_contribution = Feedback.objects.aggregate(Avg('contribution_rating'))
+    avg_communication = Feedback.objects.aggregate(Avg('communication_rating'))
+    avg_ease = Feedback.objects.aggregate(Avg('ease_of_working_together_rating'))
 
-    avg_stars = (avg_participation + avg_contribution + avg_communication + avg_ease)/4"""
+    participation = avg_participation['participation_rating__avg']
+    contribution = avg_contribution['contribution_rating__avg'] 
+    communication = avg_communication['communication_rating__avg'] 
+    ease = avg_ease['ease_of_working_together_rating__avg']
+
+    avg_stars = ( participation
+                + contribution
+                + communication
+                + ease)/4
 
