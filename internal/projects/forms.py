396aba5f4e915a1ca159a1cfa0ba828514b5361f
Andres Silva <el.ultimo.de.los.languis@gmail.com>
2013-08-14 12:19:18 -0700

    Revert "Bug fix for project creation"
    This reverts commit e894bdd08a53f2fae6acbb27349f8114732f8f18.

--- internal/projects/forms.py
+++ internal/projects/forms.py
@@ -2,9 +2,6 @@ from django.forms import *
 from projects.models import *
 
 class ProjectForm(ModelForm):
-#name = CharField(label='Project Name')
+	name = CharField(label='Project Name')
 	class Meta:
 		model = Project
-		exclude = ("collaborators" , )
-		widgets = { 'owner': HiddenInput }
-		
--- internal/projects/views.py
+++ internal/projects/views.py
@@ -4,11 +4,8 @@ from django.core.context_processors import csrf
 from django.views.generic import *
 from projects.models import *
 from projects.forms import *
-from django.contrib.auth.decorators import login_required
-from django import forms
 
 #create project page which lets you assign the project name, members, and member positions
-@login_required
 def create_project(request):
 	if request.POST:
 		form = ProjectForm(request.POST)
@@ -16,7 +13,7 @@ def create_project(request):
 			form.save()
 			return HttpResponseRedirect('/projects/')
 	else:
-		form = ProjectForm(initial={ 'widgets': { 'owner': forms.HiddenInput }, 'owner': request.user })
+		form = ProjectForm()
 
 	args = {}
 	args.update(csrf(request))
