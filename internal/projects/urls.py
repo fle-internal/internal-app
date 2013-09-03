6765e67e366f3fc3b5ad4cd5f98f3d16ce93397f
Andres Silva <el.ultimo.de.los.languis@gmail.com>
2013-09-02 19:28:00 -0700

    Revert "Project management (incomplete)"
    This reverts commit f5ff7fd7b7ea5776091bf421bec9d1729bf3e9f9.

--- internal/projects/urls.py
+++ internal/projects/urls.py
@@ -8,6 +8,4 @@ urlpatterns = patterns('',
     url(r'^$', IndexList.as_view(), name='project_index'),
     url(r'^(?P<id>\d+)/$', details, name='project_detail'),
     url(r'^create/$', create_project, name='project_create'),
-	url(r'^join(?P<id>\d+)/$', join_project, name='project_join'),
-	url(r'^edit(?P<id>\d+)/$', edit_project, name='project_edit'),
 )
