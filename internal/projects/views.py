63cc8f64905abe1d286fa2c3bd37406f4c9e4b6e
Aron Fyodor Asor <aronasorman@gmail.com>
2013-09-12 20:30:33 +0000

    fix indentation

--- internal/projects/views.py
+++ internal/projects/views.py
@@ -11,52 +11,52 @@ from projects.forms import RoleForm
 
 @login_required
 def create_project(request):
-        if request.POST:
-                form = ProjectForm(request.POST)
-                if form.is_valid():
-                        form.save()
-                        return HttpResponseRedirect('/projects/')
-        else:
-                form = ProjectForm(initial={'widgets': {'owner': forms.HiddenInput}, 'owner': request.user})
+    if request.POST:
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/projects/')
+    else:
+        form = ProjectForm(initial={'widgets': {'owner': forms.HiddenInput}, 'owner': request.user})
 
-        args = {}
-        args.update(csrf(request))
-        args['form'] = form
-        return render_to_response('projects/create_project.html', args)
+    args = {}
+    args.update(csrf(request))
+    args['form'] = form
+    return render_to_response('projects/create_project.html', args)
 
 
 class IndexList(ListView):
-        context_object_name = 'projects'
-        queryset = Project.objects.order_by('-deadline')
-        template_name = 'projects/project_index.html'
+    context_object_name = 'projects'
+    queryset = Project.objects.order_by('-deadline')
+    template_name = 'projects/project_index.html'
 
 
 @login_required
 def details(request, id):
-        detail = Project.objects.get(pk=id)
-        roles= Role.objects.filter(project=detail)
-        if request.method == 'GET':
-            form = RoleForm(initial={ 'profile': request.user, 'project': detail})
+    detail = Project.objects.get(pk=id)
+    roles= Role.objects.filter(project=detail)
+    if request.method == 'GET':
+        form = RoleForm(initial={ 'profile': request.user, 'project': detail})
+        return render(request, 'projects/project_detail.html',
+                        {'project':detail,
+                         'form': form,
+                        'roles':roles})
+    elif request.method == 'POST':
+        form = RoleForm(request.POST)
+        if form.is_valid():
+            form.save()
             return render(request, 'projects/project_detail.html',
                             {'project':detail,
                              'form': form,
                             'roles':roles})
-        elif request.method == 'POST':
-            form = RoleForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return render(request, 'projects/project_detail.html',
-                                {'project':detail,
-                                 'form': form,
-                                'roles':roles})
-            else:
-                return render(request, 'projects/project_detail.html',
-                                {'project':detail,
-                                 'form': form,
-                                'roles':roles})
+        else:
+            return render(request, 'projects/project_detail.html',
+                            {'project':detail,
+                             'form': form,
+                            'roles':roles})
 
 @login_required
 def edit_project(request, id):
-        detail = Project.objects.get(pk=id)
-        roles= Role.objects.filter(project=detail)
-        return render_to_response('projects/edit_project.html',)
+    detail = Project.objects.get(pk=id)
+    roles= Role.objects.filter(project=detail)
+    return render_to_response('projects/edit_project.html',)
