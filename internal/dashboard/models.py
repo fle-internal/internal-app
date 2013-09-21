3f34d37a6c0f77821a181f14b4a1b13ce70461c3
Aron Fyodor Asor <aronasorman@gmail.com>
2013-09-21 01:16:31 +0000

    start on News model

--- internal/dashboard/models.py
+++ internal/dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    TYPES = (('PUSH', 'push'),
+             ('PULL_REQUEST', 'pull request'))
+
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    type = models.CharField(max_length=20, choices=TYPES)
