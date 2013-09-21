240bd7523b06bde88580ccd4e2ffb9165d55a880
Aron Fyodor Asor <aronasorman@gmail.com>
2013-09-21 22:58:30 +0000

    refine News model

--- internal/dashboard/models.py
+++ internal/dashboard/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 class News(models.Model):
-    TYPES = (('PUSH', 'push'),
-             ('PULL_REQUEST', 'pull request'))
-
     title = models.CharField(max_length=25)
     description = models.TextField()
-    type = models.CharField(max_length=20, choices=TYPES)
+    type = models.CharField(max_length=20)
