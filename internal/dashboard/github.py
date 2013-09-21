f466157564df17c075007edfc15b7bdd566b6ec9
Aron Fyodor Asor <aronasorman@gmail.com>
2013-09-21 22:58:51 +0000

    save IssueEvents from github

---
+++ internal/dashboard/github.py
@@ -0,0 +1,42 @@
+import contextlib
+import json
+import urllib
+import urllib2
+
+from django.conf import settings
+from django.dispatch import receiver
+
+from github3 import login
+
+from dashboard.models import News
+from events.signals import github_issue_updated
+
+@receiver(github_issue_updated) # can be a list of signals
+def fetch_latest_github_events(sender, **kwargs):
+    data = {'client_id': settings.GITHUB_APP_ID, 'client_secret': settings.GITHUB_API_SECRET}
+    endpoint = kwargs['repo']['url'] + '/events'
+    params = urllib.urlencode(data)
+    full_url = endpoint + '?' + params
+    with contextlib.closing(urllib2.urlopen(full_url)) as req:
+        rawdata = req.read()
+        events = json.loads(rawdata)
+        return map(save_event, events)
+
+def save_event(event):
+    type = event['type']
+    if type == 'IssuesEvent':
+        return _save_issue_event(event)
+    elif type == 'IssueCommentEvent':
+        return _save_issue_comment_event(event)
+    else:
+        return None
+
+def _save_issue_event(event):
+    action = event['payload']['action']
+    actor = event['actor']['login']
+    issue_number = event['payload']['issue']['number']
+    title = "{} {} issue #{}.".format(actor, action, issue_number)
+    desc = "User {} has {} issue #{}.".format(actor, action, issue_number)
+    return News.objects.create(title=title,
+                               description=desc,
+                               type='IssuesEvent')
--- internal/dashboard/tests.py
+++ internal/dashboard/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+import json
 
 from django.test import TestCase
 
+from dashboard.github import save_event
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class IssueTestCase(TestCase):
+
+    def setUp(self):
+        self.events = {}
+        with open('fle-internal-news.json', 'r') as f:
+            rawdata = f.read()
+            self.events['fle-internal'] = json.loads(rawdata)
+            self.issue_events = [e for e in self.events['fle-internal'] if e['type'] == 'IssuesEvent']
+
+    def test_saved_event_has_correct_action_in_title(self):
+        event = self.issue_events[0]
+        for action in ['opened', 'closed', 'reopened']:
+            event['payload']['action'] = action
+            news = save_event(event)
+            self.assertRegexpMatches(news.title, action)
+
+    def test_saved_event_has_actor_in_title(self):
+        event = self.issue_events[0]
+        news = save_event(event)
+        self.assertRegexpMatches(news.title, event['actor']['login'])
+
+    def test_saved_event_has_issue_in_title(self):
+        event = self.issue_events[0]
+        news = save_event(event)
+        self.assertRegexpMatches(news.title, str(event['payload']['issue']['number']))
