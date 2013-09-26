d6a202a41ee81cd75381c956f9dd7d6a7f6e5973
Aron Fyodor Asor <aronasorman@gmail.com>
2013-09-26 21:33:33 +0000

    restore datepicker and (edit|create)_project

--- internal/projects/forms.py
+++ internal/projects/forms.py
@@ -1,5 +1,6 @@
 from django.forms import *
 from projects.models import *
+from datetime import date
 
 class RoleForm(ModelForm):
 
@@ -7,3 +8,14 @@ class RoleForm(ModelForm):
         model = Role
         widgets = { 'profile': HiddenInput,
                     'project': HiddenInput }
+
+class ProjectForm(ModelForm):
+    name = CharField(label='Project Name')
+
+    class Meta:
+        model = Project
+        exclude = ("collaborators",)
+        widgets = { 'owner': HiddenInput,
+                    'deadline': TextInput(attrs={'class': 'datepicker'}),
+                    'start_date': TextInput(attrs={'class': 'datepicker'}),
+        }
